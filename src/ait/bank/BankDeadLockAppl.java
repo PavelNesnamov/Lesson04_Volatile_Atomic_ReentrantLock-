e41617f3dab58caff2bc87adaced0075de8e75c3
Pavel Nesnamov <p.nesnamov@gmail.com>
2025-06-12 08:57:14 +0200

    branch lesson

--- src/ait/bank/BankDeadLockAppl.java
+++ src/ait/bank/BankDeadLockAppl.java
@@ -3,7 +3,7 @@ package ait.bank;
 import ait.bank.model.Account;
 import ait.bank.service.Transfer;
 
-import java.util.concurrent.locks.ReentrantLock;
+
 
 public class BankDeadLockAppl {
     public static void main(String[] args) throws InterruptedException {
@@ -12,9 +12,8 @@ public class BankDeadLockAppl {
         dad.debit(1000);
         son.debit(1000);
 
-        ReentrantLock lock = new ReentrantLock();// add ReentrantLock
-        Transfer transfer1 = new Transfer(dad, son, 900, lock);
-        Transfer transfer2 = new Transfer(son, dad, 900, lock);
+        Transfer transfer1 = new Transfer(dad, son, 900);
+        Transfer transfer2 = new Transfer(son, dad, 900);
         Thread thread1 = new Thread(transfer1);
         Thread thread2 = new Thread(transfer2);
         thread1.start();
--- src/ait/bank/service/Transfer.java
+++ src/ait/bank/service/Transfer.java
@@ -4,23 +4,19 @@ import java.util.concurrent.locks.ReentrantLock;
 
 public class Transfer implements Runnable {
 
-    ReentrantLock locker; // add ReentrantLock lock as variable
     private Account accFrom;
     private Account accTo;
     private int sum;
 
-    public Transfer(Account accFrom, Account accTo, int sum, ReentrantLock lock) {
+    public Transfer(Account accFrom, Account accTo, int sum) {
         this.accFrom = accFrom;
         this.accTo = accTo;
         this.sum = sum;
-        locker = lock;  // add ReentrantLock to constructor
     }
 
 
     @Override
     public void run() {
-
-        locker.lock(); //blocking
 
         synchronized (accFrom) {
             try {
@@ -28,9 +24,7 @@ public class Transfer implements Runnable {
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
-            finally {
-                locker.unlock(); //unblocked
-            }
+
             synchronized (accTo) {
                 if (accFrom.getBalance() >= sum) {
                     accFrom.credit(sum);
